d6a8c2546b8b23dea13bd1b4480b346927f48ad0
Daniel Antrim <daniel.joseph.antrim@cern.ch>
2015-11-17 17:35:41 +0100

    include

---
+++ Readout_Software/include/configuration_module.h
@@ -0,0 +1,153 @@
+#ifndef CONFMOD_H
+#define CONFMOD_H
+
+#include <QSettings>
+#include <QFile>
+#include <QByteArray>
+#include <QUdpSocket>
+#include <QBitArray>
+#include <QStringList>
+#include <QDomDocument>
+
+using namespace std;
+
+class Configuration : public QObject
+{
+
+  Q_OBJECT
+
+    public: 
+
+        Configuration();
+        ~Configuration();
+        int Ping();
+        int ReadCFile(QString &filename);
+        int SendConfig();
+        void Sender(QByteArray blockOfData, const QString &ip, quint16 destPort);
+        void Sender(QByteArray blockOfData, const QString &ip, quint16 destPort, QUdpSocket& socket);
+        quint32 ValueToReplace(QString datagramHex, int bitToChange, bool bitValue);
+        int ResetTriggerCounter();
+        QByteArray bitsToBytes(QBitArray bits);
+
+        //Getters
+        QString getGain() {return _gainstring;}
+        int getPeakTime() {return _peakint;}
+        QString getNeighborTrigger() {return _ntrigstring;}
+        void getIPs(QStringList &list) {list=_ips;}
+        void getVMMIDs(QStringList &list) {list=_ids;}
+        QString getComment() {return _comment;}
+        int getThresholdDAC() {return _thresDAC;}
+        int getTestPulseDAC() {return _tpDAC;}
+        quint16 getChannelMap() {return channelMap;} 
+        void getTrigDAQ(QStringList &slist, vector<int> &ilist){
+	    	slist << Trig_Period;
+	    	slist << Run_mode;
+	    	slist << Out_Path;
+	    	slist << Out_File;
+	    	ilist.clear();
+	    	ilist.push_back(TP_Delay);
+	    	ilist.push_back(ACQ_Sync);
+	    	ilist.push_back(ACQ_Window);
+	    	ilist.push_back(Run_count);
+        }
+
+        quint16 getFECPort()      { return FECPort; }
+        quint16 getDAQPort()      { return DAQPort; }
+        quint16 getVMMASICPort()  { return VMMASICPort; }
+        quint16 getVMMAPPPort()   { return VMMAPPPort; }
+        quint16 getS6Port()       { return S6Port; }
+
+    private:
+
+        QStringList _ips;
+        QStringList _ids;
+        QStringList _replies;
+        QHostAddress* boardip;
+        quint16 FECPort, DAQPort, VMMASICPort, VMMAPPPort, S6Port;
+        quint32 commandCounter;
+        bool pinged;
+        bool bnd;
+        QUdpSocket* socket;
+        QByteArray buffer;
+
+        bool debug;
+	    QSettings* settings;
+	    QDomNode *n; 
+	    QDomNode n_sub;
+	    QDomNodeList hdmis;
+	    QFile *file;
+
+	    //Trigger.DAQ setting
+	    int TP_Delay, ACQ_Sync, ACQ_Window, Run_count;
+	    QString Trig_Period, Run_mode, Out_Path, Out_File;
+
+	    //Channel Map settings
+	    bool hdmisw; 
+	    QString hdmifi, hdmise;
+	    quint16 channelMap;
+
+        //Global settings
+	    int _chSP;//ch.polarity
+        QString _comment;
+        int _gain;//gain
+        QString _gainstring;
+        int _peakt;//peak.time
+        int _peakint;
+        int _art;//ART
+        int _artm;//ART.mode
+        int _cmon;//channel.monitoring
+        int _scmx;//monitoring control
+        int _sbmx;//monitor.pdo.out
+        int _tpDAC;//test.pulse.DAC
+        int _thresDAC;//threshold.DAC
+        int _ntrig;//neighbor.trigger
+        QString _ntrigstring;
+        int _nanaltri;//analog.tristates
+	    int _leak;//ch.leakage.current
+	    int _doubleleak;//double.leakage
+	    int _dualclock;//dual.clock.ART
+        int _adcs;//ADCs
+        int _TACslop;//TAC.slop.adj
+        int _hyst;//sub.hyst.discr
+        int _dtime;//direct.time
+        int _dtimeMode[2];//direct.time.mode
+        int _sbfm;//out.buffer.mo
+        int _sbfp;//out.buffer.pdo
+        int _sbft;//out.buffer.tdo
+        int _dpeak;//disable.at.peak
+	    int _ebitconvmode;//conv.mode.8bit
+	    int _sbitenable;//enable.6bit
+	    int _adc10b;//ADC.10bit
+	    int _adc8b;//ADC.8bit
+	    int _adc6b;//ADC.6bit
+	    int _dualclockdata;//dual.clock.data
+	    int _dualclock6bit;//dual.clock.6bit
+
+        //Channel-by-channel info
+        bool SMX[64];//smx
+        int trim[64];//trim
+        int tenbADC[64];//10bADCtimeset
+        int eightbADC[64];//08bADCtimeset
+        int sixbADC[64];//06bADCtimeset
+        bool SM[64];//hidden.mode
+        bool SP[64];//polarity
+        bool SC[64];//sc
+        bool SL[64];//sl
+        bool ST[64];//test.pulse
+ 
+        //Utilies
+        void UpdateCounter();
+        quint32 reverse32(QString datagramHex);
+        bool EnaDisableKey( const QString &childKey, const QString &tmp);
+        bool EnaDisableKey( const QString &childKey, const QString &tmp, int channel);
+        bool OnOffKey( const QString &childKey, const QString &tmp);
+        bool OnOffChannel( const QString &key, const QString &tmp, int channel);
+
+        void Validate(QString sectionname);
+
+  private slots:
+        void processReply(const QString &_sentip, QUdpSocket* socket);
+        void dumpReply(const QString &_sentip);
+};
+
+#endif
---
+++ Readout_Software/include/run_module.h
@@ -0,0 +1,103 @@
+#ifndef RUN_MODULE_H
+#define RUN_MODULE_H
+
+//qt
+#include <QObject>
+#include <QFile>
+#include <QStringList>
+#include <QByteArray>
+#include <QMap>
+#include <QList>
+#include <QString>
+#include <QDir>
+
+//vmmrun
+#include "configuration_module.h"
+
+class RunDAQ : public QObject
+{
+    Q_OBJECT
+
+    public :
+        explicit RunDAQ(QObject *parent = 0);
+        virtual ~RunDAQ(){};
+        void UpdateCounter();
+        void SetDebugMode(bool doDbg) { m_dbg = doDbg; }
+        void SetTestMode(bool doTest) { m_is_testmode = doTest; }
+        void SetOutputFileName(QString name) { m_useCustomName = true; m_userGivenName = name; }
+        void ReadRFile(QString &file);
+
+        // methods to set up the DAQ from the information read in ReadRFile
+        void PrepareRun();
+        void SetTrigAcqConstants();
+        void SetTriggerMode();
+        void ACQOn();
+        void ACQOff();
+
+        // event loop method
+        void DataHeader();
+        void TimedRun(long time); 
+        void PulserRun();
+
+        // method to send datagrams to the boards
+        void Sender(QByteArray datagram_);
+
+    private :
+        // global configuration parameters (c.f. vmmconfig/configuration_module.h)
+        Configuration* m_config;
+
+        bool m_has_config;
+        bool m_is_testmode;
+        bool m_dbg;
+        quint32 n_command_counter; // keep track of number of commands passed to DAQ
+
+        QUdpSocket* m_socket;
+        QUdpSocket* m_socketDAQ;
+        bool pinged;
+        bool bound;
+        QStringList m_replies;
+        QByteArray m_buffer;
+        QByteArray m_bufferDAQ;
+
+        // configuration parameters
+        int m_tpdelay;
+        int m_acqsync;
+        int m_acqwindow;
+        int m_runcount;
+        bool m_timedrun;
+        QString m_trigperiod;
+        QString m_runmode;
+        QString m_infilename;
+        QString m_outfilepath;
+        QString m_outfilename;
+        bool m_useCustomName;
+        QString m_userGivenName;
+
+        // channel map (which HDMI's to interact with)
+        quint16 m_channelmap;
+
+        QStringList m_ips;
+
+        // output file
+        QFile m_fileDaq;
+
+        // pulser handling
+        int n_pulse_count;
+
+    signals :
+        void ExitLoop();
+
+    public slots :
+
+    private slots :
+        void Run();
+        void processReply(const QString &queried_ip, int command_delay = 0);
+        void ReadEvent();
+        void SendPulse();
+        void FinishRun();
+
+
+}; // class RunDAQ
+
+
+#endif
