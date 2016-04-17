5663fed68a7d08c4a023c31b5cab89d8d520161c
Daniel Antrim <daniel.joseph.antrim@cern.ch>
2016-04-17 20:04:19 +0200

    move old ReadoutStructre

--- Readout_Software/include/LinkDef.h
+++
@@ -1,9 +0,0 @@
-#ifdef __CINT__
-#pragma link off all globals;
-#pragma link off all classes;
-#pragma link off all functions;
-#pragma link C++ class vector<int>+;
-//#pragma link C++ class vector<double>+;
-#pragma link C++ class vector<vector<int> >+;
-#pragma link C++ class vector<vector<double> >+;
-#endif
